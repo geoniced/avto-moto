a0dcb7cfe270a626aec7fb16a0d1f164690b866b
Ivan Kashin <barbos1701@gmail.com>
2021-03-13 12:00:00 +0300

    Добавляет Helmet в index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Helmet from "react-helmet";
 import App from "./components/app/app";
+import openSansRegularWoff2 from "./fonts/open-sans-regular.woff2";
 import "./scss/style.scss";
 
 ReactDOM.render(
-    <React.StrictMode>
+    <>
+      <Helmet>
+        <link rel="preload" href={openSansRegularWoff2} as="font" type="font/woff2" crossOrigin/>
+      </Helmet>
       <App />
-    </React.StrictMode>,
+    </>,
     document.getElementById(`root`)
 );
